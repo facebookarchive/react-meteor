48fb6424509a2824eb034ab91b1699bf7301ccf0
Ben Newman <ben@meteor.com>
2015-04-08 18:07:11 -0400

    Respect path of process.env.ROOT_URL when injecting React.
    Fixes #61.

--- src/inject-react.js
+++ src/inject-react.js
@@ -2,7 +2,12 @@ var file = process.env.NODE_ENV === "production"
   ? "react-with-addons-0.13.0.min.js"
   : "react-with-addons-0.13.0.js";
 
+var src = Npm.require("path").join(
+  Npm.require("url").parse(process.env.ROOT_URL).path,
+  "packages/reactjs_react/vendor/" + file
+);
+
 Inject.rawHead(
   'react',
-  '<script src="/packages/reactjs_react/vendor/' + file + '"></script>'
+  '<script src="' + src + '"></script>'
 );
