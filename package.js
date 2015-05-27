42a61f3ef76861b728c6e35dd865e5af3b046181
Ben Newman <ben@meteor.com>
2015-05-27 17:42:17 -0400

    Bump package version to 0.2.4 in preparation for publishing.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "reactjs:react",
   // TODO Consider using reactVersion here, since this version is a lot
   // less meaningful?
-  version: "0.2.3",
+  version: "0.2.4",
   summary: "React rendering for Meteor apps",
   git: "https://github.com/reactjs/react-meteor/",
   documentation: "README.md"
